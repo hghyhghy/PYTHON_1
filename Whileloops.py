50bcd7f2546d9bc189d3da46b7446536f04e6e42
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-09-29 02:17:17 +0530

    Add files via upload

---
+++ Whileloops.py
@@ -0,0 +1,36 @@
+
+
+k=0
+
+while(k<=9):
+      
+       print(k)
+
+       k=k+1
+
+
+i= int (input("Enter a number "))
+
+while i<=30:
+
+       i= int (input("Enter a number "))
+
+       print(i)
+
+print("Done with the loop")       
+       
+
+
+# decrementing while loop
+
+count=8
+
+while count>0:
+
+       print(count)
+
+       count=count-1
+
+else:
+       print("The loop has been terminated ")
+       
