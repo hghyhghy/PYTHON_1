e0e9a815b36bdf960dc8065cac928c935946a56b
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-09-29 01:45:57 +0530

    Add files via upload

---
+++ Forloops.py
@@ -0,0 +1,47 @@
+# iterating through for loop
+
+name = "SubhamSarkar"
+
+for i in name:
+    print(i, end=",")
+
+    if(i=="k"):
+        
+        print("HeHe!")
+
+
+# for loops for list 
+
+color=["Red" ,"Black" , "White" ,"Blue","Orange"]
+
+for col in color:
+    
+    print(col)
+
+    for i in col:
+        
+        print(i)
+
+# printng numbers using for loop
+
+for  x in range(7):
+    
+    print(x)
+
+
+for y in range(6):
+
+    print(y+1)
+    
+
+for z in range(1,9):
+    
+    print(z)
+
+
+# rang() for 3 arguments 
+
+
+for d in range(1,12,4):
+    
+    print(d)
